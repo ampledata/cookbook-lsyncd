ee12b78b8cf68c37cb91a8b1e5933bd3bf9456f9
Greg Albrecht <gba@gregalbrecht.com>
2013-03-12 11:55:32 -0700

    Bumped version from 1.0.0 to 1.1.0.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email 'gba@gregalbrecht.com'
 license          'Apache License 2.0'
 description      'Installs/Configures lsyncd.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
 depends 'supervisor'
